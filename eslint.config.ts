fcc45e5b10e6e8acead4b7529095277b4f0cfa16
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2026-05-10 19:32:32 -0300

    chore(eslint): ignore coverage and node_modules dirs

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,7 +7,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { ignores: ["dist/**"] },
+  { ignores: ["dist/**", "coverage/**", "node_modules/**"] },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
