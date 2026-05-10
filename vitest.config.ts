7f88f4adee18c66bef3edcd02ee53c373a8862ed
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2026-05-10 19:32:49 -0300

    chore(test): remove @vitest/coverage-v8 and coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,25 +4,20 @@ import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
+  resolve: {
     alias: [
+      {
+        find: "@/test",
+        replacement: resolve(__dirname, "test"),
+      },
       {
         find: "@",
         replacement: resolve(__dirname, "src"),
       },
     ],
+  },
+  test: {
     globals: true,
-    coverage: {
-      provider: "v8",
-      include: [
-        "src/modules/**/domain/**",
-        "!src/core/entity/dto.ts",
-        "src/modules/**/application/{subscribers,use-cases}/**",
-        "!src/modules/**/application/use-cases/factories/**",
-        "!src/modules/**/application/use-cases/dtos/**",
-        "!src/modules/**/domain/events/**",
-      ],
-    },
     include: ["src/**/*.spec.ts", "!src/**/*.e2e.spec.ts"],
     setupFiles: ["/test/container/index.ts"],
   },
