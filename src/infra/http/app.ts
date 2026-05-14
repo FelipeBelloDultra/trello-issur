65efbf538512bef5644807af3d4be31cb1a703ed
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2026-05-14 14:54:27 -0300

    feat(http): wire logger plugin into Elysia app

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -3,12 +3,14 @@ import { openapi } from "@elysiajs/openapi";
 import { Elysia } from "elysia";
 
 import { container } from "@/infra/container";
+import { TOKENS } from "@/infra/container/tokens";
 import { RegisterUserController } from "@/modules/user/infra/http/controllers/register-user.controller";
 
-import { TOKENS } from "../container/tokens";
+import { loggerPlugin } from "./plugins/logger.plugin";
 
 export const createApp = () =>
   new Elysia({ adapter: node() })
+    .use(loggerPlugin)
     .use(openapi())
     .get("/health", () => ({ status: "ok" }))
     .use(container.get<RegisterUserController>(TOKENS.RegisterUserController).setup());
