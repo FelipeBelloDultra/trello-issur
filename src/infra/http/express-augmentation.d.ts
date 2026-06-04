b2f0329d363fac69a29c866e7694a284038f54b0
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2026-06-04 20:34:49 -0300

    feat(http): add FileUploadMiddleware with busboy

--- src/infra/http/express-augmentation.d.ts
+++ src/infra/http/express-augmentation.d.ts
@@ -1,9 +1,11 @@
 import "express";
 
+import { UploadedFile } from "@/infra/http/middlewares/file-upload.middleware";
 import { TokenClaims } from "@/modules/auth/domain/value-objects/token-claims";
 
 declare module "express" {
   interface Request {
     account?: TokenClaims;
+    uploadedFile?: UploadedFile;
   }
 }
